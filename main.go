bd064dd1af2d57ed2782a327e5c2414ccd2ac051
elaron <elarwei@gmail.com>
2018-05-13 21:02:08 +0800

    define client and server structure
    Signed-off-by: elaron <elarwei@gmail.com>

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 	"time"
 )
 
-type ReqId string
+type Req struct {
+	ClientName string
+	ReqId      int
+}
+
 type Client struct {
-	Name     string  `json:"name"`
-	Speed    int     `json:"speed"`
-	Wr       int     `json:"reservation"`
-	Wl       int     `json:"limit"`
-	Requests []ReqId `json:"requests"`
+	Name     string `json:"name"`
+	Speed    int    `json:"speed"`
+	Wr       int    `json:"reservation"`
+	Wl       int    `json:"limit"`
+	Requests []Req  `json:"requests"`
 }
 
 var g_reqId int
@@ -26,18 +30,17 @@ func NewId() int {
 }
 
 func (c *Client) RequestGenerator(reqCh chan string) {
-	for {
-		step := 1000 / c.Speed
-		ticker := time.NewTicker(time.Duration(step) * time.Microsecond)
-		for range ticker.C {
-			reqCh <- c.Name
-		}
+	step := 1000 / c.Speed
+	fmt.Println(*c)
+	ticker := time.NewTicker(time.Duration(step) * time.Millisecond)
+	for range ticker.C {
+		reqCh <- c.Name
 	}
 }
 
 type Server struct {
-	Capacity      int     `json:"capacity"`
-	WaitQueue     []ReqId `json:"wait_queue"`
+	Capacity      int   `json:"capacity"`
+	WaitQueue     []Req `json:"wait_queue"`
 	waitQueueLock sync.RWMutex
 }
 
@@ -45,19 +48,32 @@ func (s *Server) Enqueue(reqCh chan string) {
 	for {
 		select {
 		case client := <-reqCh:
-			s.waitQueueLock.Lock()
-			s.WaitQueue = append(s.WaitQueue, ReqId(fmt.Sprintf("%s.%d", client, NewId())))
-			s.waitQueueLock.Unlock()
+			//s.waitQueueLock.Lock()
+			s.WaitQueue = append(s.WaitQueue, Req{client, NewId()})
+			//s.waitQueueLock.Unlock()
 		}
 	}
 }
 
-func (s *Server) PrintQueue() {
-	s.waitQueueLock.RLock()
+func (s *Server) FIFODequeue() {
+	counter := make(map[string]int)
+	max := s.Capacity
+	//s.waitQueueLock.RLock()
 	for _, v := range s.WaitQueue {
-		fmt.Println(v)
+		if max <= 0 {
+			break
+		}
+		//fmt.Println(v)
+		n, ok := counter[v.ClientName]
+		if !ok {
+			counter[v.ClientName] = 1
+		} else {
+			counter[v.ClientName] = n + 1
+		}
+		max -= 1
 	}
-	s.waitQueueLock.RUnlock()
+	//s.waitQueueLock.RUnlock()
+	fmt.Println(len(s.WaitQueue), counter)
 }
 
 var g_clientList []*Client
@@ -66,20 +82,20 @@ var g_server Server
 func init() {
 	g_reqId = 0
 	g_clientList = []*Client{
-		&Client{Name: "a", Speed: 50, Wr: 20, Wl: 70, Requests: []ReqId{}},
-		&Client{Name: "b", Speed: 52, Wr: 30, Wl: 60, Requests: []ReqId{}},
-		&Client{Name: "c", Speed: 40, Wr: 10, Wl: 70, Requests: []ReqId{}},
+		&Client{Name: "a", Speed: 800, Wr: 20, Wl: 70, Requests: []Req{}},
+		&Client{Name: "b", Speed: 52, Wr: 30, Wl: 60, Requests: []Req{}},
+		&Client{Name: "c", Speed: 40, Wr: 10, Wl: 70, Requests: []Req{}},
 	}
-	g_server = Server{Capacity: 100, WaitQueue: []ReqId{}}
+	g_server = Server{Capacity: 100, WaitQueue: []Req{}}
 }
 
 func main() {
 	reqCh := make(chan string, 100)
+	go g_server.Enqueue(reqCh)
 	for _, client := range g_clientList {
 		go client.RequestGenerator(reqCh)
 	}
-	go g_server.Enqueue(reqCh)
 
 	time.Sleep(1 * time.Second)
-	g_server.PrintQueue()
+	g_server.FIFODequeue()
 }
